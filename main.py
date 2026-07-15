9f1e6d291d58439cfb0042676d7172c8b20db7ce
prathitnarayan <pnsprat411@gmail.com>
2026-07-16 01:21:09 +0530

    Add Hugging Face Space metadata

--- main.py
+++ main.py
@@ -713,7 +713,7 @@ async def api_endpoint(req: ApiRequest, background_tasks: BackgroundTasks):
 @app.get("/", response_class=HTMLResponse)
 async def root():
     """Serve the UI"""
-    return Path("/app/index.html").read_text()
+    return Path("/app/static/index.html").read_text()
 
 @app.get("/info")
 async def info():
